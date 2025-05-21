9c954eb053df5b8278636b23f9033ddccee5f74d
sanghak yi <sang105kr@gmail.com>
2025-05-21 15:46:42 +0900

    20250521-6

--- src/main/resources/static/js/product/csr/product.js
+++ src/main/resources/static/js/product/csr/product.js
@@ -1,12 +1,13 @@
-const $button = document.createElement('button');
-$button.textContent = '목록 가져오기';
+// const $button = document.createElement('button');
+// $button.textContent = '목록 가져오기';
 
-$button.addEventListener('click',e=>{
-  productList()
-});
+// $button.addEventListener('click',e=>{
+//   productList()
+// });
 
-const $body = document.querySelector('body');
-$body.append($button);
+// const $body = document.querySelector('body');
+// $body.append($button);
+
 
 async function productList () {
 
@@ -28,11 +29,43 @@ async function productList () {
     }
     const json = await res.json();  //응답메세지 body내 json포맷문자열은 js객체로 변환
     console.log(json);
-    console.log(json.header);
-    console.log(json.body);
-    console.log(json.paging);
+
+    //상품목록화면
+    displayProductList(json.body);
+
   }catch (err){
     console.error(err.message);
   }  
 
 }
+
+function displayProductList(list) {
+
+  const $div = document.createElement('div');
+  const makeTr = list => list.map(product =>
+      `<tr>
+          <td>${product.productId}</td>
+          <td>${product.pname}</td>      
+      </tr>`
+    ).join('');
+
+  $div.innerHTML = `
+    <table>
+      <caption>상품목록</caption>
+      <thead>
+        <tr>
+          <th>상품번호</th>
+          <th>상품명</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${makeTr(list)}
+      </tbody>
+    </table>
+  `;
+
+  document.body.append($div);
+}
+
+
+productList();
