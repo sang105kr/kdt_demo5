57236be4e89aaa781c32c1a1467833005733b357
sanghak yi <sang105kr@gmail.com>
2025-07-02 17:31:06 +0900

    20250702-8

--- src/main/java/com/kh/demo/web/form/bbs/SaveForm.java
+++ src/main/java/com/kh/demo/web/form/bbs/SaveForm.java
@@ -1,13 +1,17 @@
 package com.kh.demo.web.form.bbs;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import javax.annotation.RegEx;
 
 @Data
 public class SaveForm {
   @NotBlank(message = "제목은 필수 입력 항목입니다.")
   @Size(min = 3, max=50, message="제목은 최소3자 최대50자 이내로 입력해 주세요.")
+  @Pattern(regexp = "\\w|[가-힣]", message = "문자만 입력가능합니다.")
   private String title;
   @NotBlank(message = "내용은 필수 입력 항목입니다.")
   private String content;
